672a7502878dbdc29cd81024aaa6eaefe986d3f3
Mahas1 <mahasvan@gmail.com>
2023-06-26 19:16:16 +0530

    use Slash Command Groups

--- cogs/host.py
+++ cogs/host.py
@@ -3,12 +3,16 @@ from discord.ext import commands
 
 import platform
 
+
 class Host(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name="info", description="Info about the machine in which I am hosted.", guild_ids=[861259655011237939, 830456831183945778])
-    async def hostinfo(self, interaction: discord.Interaction):
+    host = discord.SlashCommandGroup("host", description="Commands related to the host machine.")
+
+    @host.command(name="info", description="Info about the machine in which I am hosted.",
+                  guild_ids=[861259655011237939, 830456831183945778])
+    async def host_info(self, interaction: discord.Interaction):
         data = await self.bot.internet.get_json("/host/info")
         embed = discord.Embed(
             title=data.get("hostname", "Host Info"),
@@ -17,6 +21,7 @@ class Host(commands.Cog):
                         f"## Back-end"
         )
         embed.add_field(name="OS", value=data["os"], inline=False)
+        embed.add_field(name="CPU", value=data["cpu"], inline=False)
         embed.add_field(name="CPU Threads", value=data["cpu_threads"], inline=False)
         embed.add_field(name="CPU Usage", value=f"{data['cpu_usage']}%", inline=False)
         embed.add_field(name="Memory Usage", value=f"{data['memory_usage']}%", inline=False)
@@ -25,4 +30,3 @@ class Host(commands.Cog):
 
 def setup(bot):
     bot.add_cog(Host(bot))
-
--- cogs/translation.py
+++ cogs/translation.py
@@ -13,12 +13,14 @@ class Translate(commands.Cog):
         loop = asyncio.get_event_loop()
         loop.create_task(self.populate_langs())
 
+    translation = discord.SlashCommandGroup("translation", description="Commands for translating text")
+
     async def populate_langs(self):
         self.lang_dict = await self.bot.internet.get_json("/translate/languages")
         self.lang_dict = self.lang_dict.get("response")
 
-    @commands.slash_command(name="translate", description="Translate text to another language",
-                            guild_ids=[861259655011237939])
+    @translation.command(name="translate", description="Translate text to another language",
+                         guild_ids=[861259655011237939])
     async def translate(self, interaction: discord.Interaction,
                         message: str, from_language: str = "auto", to_language: str = "en"):
         response = await self.bot.internet.post_json("/translate/translate",
@@ -36,8 +38,8 @@ class Translate(commands.Cog):
 
         await interaction.response.send_message(embed=embed)
 
-    @commands.slash_command(name="detect", description="Detect the language of a text",
-                            guild_ids=[861259655011237939])
+    @translation.command(name="detect", description="Detect the language of a text",
+                         guild_ids=[861259655011237939])
     async def detect(self, interaction: discord.Interaction, message: str):
         response = await self.bot.internet.post_json("/translate/detect",
                                                      data=json.dumps({"text": message}))
@@ -48,8 +50,12 @@ class Translate(commands.Cog):
             color=interaction.user.color,
         )
         embed.add_field(name="Source Text", value=message, inline=False)
-        embed.set_footer(text=f"Confidence: {response.get('confidence')}%")
+        confidence = round(response.get("confidence"), 2)
+        embed.set_footer(text=f"Confidence: {confidence}%")
         await interaction.response.send_message(embed=embed)
+
+    # TODO: list all languages
+
 
 def setup(bot):
     bot.add_cog(Translate(bot))
